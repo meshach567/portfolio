429236181ee6c79705e76615085ec0caa518aa21
meshach567 <arinzemeshach123@gmail.com>
2024-09-03 02:16:14 +0100

    commit

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,11 +1,9 @@
-import React, { useContext, useRef, useState } from "react";
+import React, {  useRef, useState } from "react";
 import "./Contact.css";
 import emailjs from "@emailjs/browser";
-import { themeContext } from "../../Context";
+
 
 const Contact = () => {
-  const theme = useContext(themeContext);
-  const darkMode = theme.state.darkMode;
   const form = useRef<HTMLFormElement>(null);
   const [done, setDone] = useState(false);
 
